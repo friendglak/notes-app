c23e38386131eb1c77091255373732560bf30d93
Juan Camilo Velásquez Amarillo <camilojheans@gmail.com>
2023-12-04 12:11:08 -0500

    Add: Update fonts and add note link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        Notes App
-        <Link href="/notes"> Notes</Link>
+      <main className="flex min-h-screen flex-col items-center justify-center p-24 text-6xl">
+        Notes App 📝
+        <Link className="text-7xl text-center text-red-400" href="/notes">
+          ¡Entra aquí para agregar tu nota!
+        </Link>
       </main>
     </>
   );
